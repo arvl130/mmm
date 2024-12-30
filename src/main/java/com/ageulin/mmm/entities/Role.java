58db3daab3216d51ea7ffc5a46d0a9f6d0947e4d
Angelo Geulin <angelogeulin123@gmail.com>
2024-12-30 13:25:34 +0800

    fix: exclude relations and sensitive fields from Jackson, Lombok serialization
    This also fixes an issue where we are getting infinite recursion when serializing
    to JSON because of nested relations.
    
    For more info, see: https://stackoverflow.com/a/56268015

--- src/main/java/com/ageulin/mmm/entities/Role.java
+++ src/main/java/com/ageulin/mmm/entities/Role.java
@@ -1,12 +1,15 @@
 package com.ageulin.mmm.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.util.List;
 
-@Entity
 @Data
+@Entity
 @Table(name = "roles")
 public class Role {
     @Id
@@ -16,6 +19,9 @@ public class Role {
     @Column(nullable = false)
     private String name;
 
+    @JsonIgnore
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @ManyToMany(mappedBy = "roles")
     private List<User> users;
 }
--- src/main/java/com/ageulin/mmm/entities/User.java
+++ src/main/java/com/ageulin/mmm/entities/User.java
@@ -1,13 +1,16 @@
 package com.ageulin.mmm.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.util.List;
 import java.util.UUID;
 
-@Entity
 @Data
+@Entity
 @Table(name = "users")
 public class User {
     @Id
@@ -20,9 +23,17 @@ public class User {
     @Column(nullable = false)
     private String email;
 
+    // We never want this to be serialized (by Jackson or Lombok)
+    // because it is sensitive data.
+    @JsonIgnore
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @Column(nullable = false)
     private String password;
 
+    @JsonIgnore
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @ManyToMany
     @JoinTable(
         name = "user_roles",
@@ -30,4 +41,10 @@ public class User {
         inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
     )
     private List<Role> roles;
+
+    @JsonIgnore
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Meme> memes;
 }
