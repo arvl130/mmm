6493972fefd34f8675c05fdd5b1d6ce1c07142e2
Angelo Geulin <angelogeulin123@gmail.com>
2025-01-01 03:32:52 +0800

    fix: match cookie max age to session max inactive interval
    This fixes an issue where sessions are invalidated prematurely.
    
    By default, sessions saved by Spring Session are only valid for
    30 minutes (based on Tomcat server servlet defaults). This causes
    an issue where cookies remain in the client browser, despite the
    session it is referencing already being invalidated.
    
    To fix this, we extend the session timeout to 7 days (the same as
    the cookie max age) so that both the cookie and the session it is
    referencing can be invalidated at the same time.
    
    For more info, see:
    [1] https://docs.spring.io/spring-boot/reference/web/spring-session.html
    [2] https://www.codejava.net/frameworks/spring-boot/change-session-timeout

--- src/main/java/com/ageulin/mmm/config/SessionConfig.java
+++ src/main/java/com/ageulin/mmm/config/SessionConfig.java
@@ -7,24 +7,34 @@ import org.springframework.session.jdbc.JdbcIndexedSessionRepository;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
+import java.time.Duration;
+
 @Configuration
 public class SessionConfig {
+    private final Duration SESSION_VALID_DURATION = Duration.ofDays(7);
 
     @Bean
     public SessionRepositoryCustomizer<JdbcIndexedSessionRepository> sessionRepositoryCustomizer() {
-        return sessionRepository -> sessionRepository
+        return sessionRepository -> {
             // The name "sessions" would be preferred here, but unfortunately the
             // name we specify is expanded to [name]_attributes under the hood,
             // so I've opted not to pluralize it.
-            .setTableName("session");
+            sessionRepository.setTableName("session");
+            // This should be greater than or equal to the cookie max age.
+            // Otherwise, we will have cookies saved on client browsers
+            // referencing a session that no longer exists.
+            //
+            // Ideally, cookies and its corresponding session should be
+            // invalidated at the same time. Hence, we make them match here.
+            sessionRepository.setDefaultMaxInactiveInterval(this.SESSION_VALID_DURATION);
+        };
     }
 
     @Bean
     public CookieSerializer cookieSerializer() {
         var serializer = new DefaultCookieSerializer();
-        var SEVEN_DAYS = 7 * 24 * 60 * 60;
 
-        serializer.setCookieMaxAge(SEVEN_DAYS);
+        serializer.setCookieMaxAge(Math.toIntExact(this.SESSION_VALID_DURATION.toSeconds()));
         return serializer;
     }
 }
