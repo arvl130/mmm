456c73462d963f9eebe3834172492726e70cd421
Angelo Geulin <angelogeulin123@gmail.com>
2025-01-05 00:46:45 +0800

    fix: use Identity generation type for auto-incrementing primary key
    The default evaluates to Sequence generation type which is
    not able to find the correct PostgreSQL sequence.

--- src/main/java/com/ageulin/mmm/entities/Keyword.java
+++ src/main/java/com/ageulin/mmm/entities/Keyword.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Table(name = "keywords")
 public class Keyword {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
 
     @Column(nullable = false, unique = true)
