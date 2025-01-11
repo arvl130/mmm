79ac62cd61575b423f7edb4b204f381955d740a7
Angelo Geulin <angelogeulin123@gmail.com>
2025-01-11 20:39:48 +0800

    feat: add endpoint for getting keyword suggestions on existing memes

--- src/main/java/com/ageulin/mmm/controllers/KeywordController.java
+++ src/main/java/com/ageulin/mmm/controllers/KeywordController.java
@@ -1,27 +1,51 @@
 package com.ageulin.mmm.controllers;
 
+import com.ageulin.mmm.config.SecurityUser;
 import com.ageulin.mmm.dtos.responses.KeywordSuggestionResponse;
+import com.ageulin.mmm.exceptions.HttpPreconditionFailedException;
 import com.ageulin.mmm.exceptions.HttpTooManyRequestsException;
+import com.ageulin.mmm.repositories.MemeRepository;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ServerErrorException;
 import org.springframework.web.server.ServerWebInputException;
 import org.springframework.web.server.UnsupportedMediaTypeStatusException;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
 import software.amazon.awssdk.services.bedrockruntime.model.*;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/keywords")
+@RequestMapping("/api/v1")
 public class KeywordController {
-    @PostMapping("/suggestions")
+    private static final String prompt = """
+        You are an expert at analyzing images.
+        Extract the most relevant keywords that best represent the image's content.
+
+        Respond with a comma-separated list of keywords, with no extra text.
+        Example: foo, bar, baz
+    """;
+
+    private final MemeRepository memeRepository;
+
+    public KeywordController(MemeRepository memeRepository) {
+        this.memeRepository = memeRepository;
+    }
+
+    @PostMapping("/keywords/suggestions")
     public ResponseEntity<KeywordSuggestionResponse> getSuggestions(
         @NotNull @RequestParam("file") MultipartFile file
     ) {
@@ -29,39 +53,96 @@ public class KeywordController {
             throw new ServerWebInputException("Input file is required.");
         }
 
-        var imageFormat = switch (file.getContentType()) {
+        try {
+            var keywords = getKeywordSuggestions(file.getBytes(), file.getContentType());
+            return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new KeywordSuggestionResponse(
+                    "Received reply from AI model.",
+                    censorAWSAccountNumber(
+                        System.getenv("AWS_BEDROCK_MODEL_ID")
+                    ),
+                    prompt,
+                    file.getContentType(),
+                    file.getOriginalFilename(),
+                    keywords
+                ));
+        } catch (IOException e) {
+            throw new ServerErrorException("Could not get file bytes.", e);
+        }
+    }
+
+    @GetMapping("/memes/{id}/keyword-suggestions")
+    public ResponseEntity<KeywordSuggestionResponse> getEditSuggestions(
+        @AuthenticationPrincipal SecurityUser securityUser,
+        @PathVariable UUID id
+    )
+    {
+        var meme = this.memeRepository
+            .findByIdAndUserId(id, securityUser.getId())
+            .orElseThrow(() -> new ServerWebInputException("No such meme."));
+
+        try (var s3Client = S3Client.builder().build()) {
+            var inputStream = s3Client.getObject(builder -> builder
+                .bucket(System.getenv("AWS_S3_BUCKET"))
+                .key("memes/" + meme.getId())
+                .build()
+            );
+
+            var imageBytes = inputStream.readAllBytes();
+            var imageContentType = inputStream.response().contentType();
+            var keywords = getKeywordSuggestions(imageBytes, imageContentType);
+
+            return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new KeywordSuggestionResponse(
+                    "Received reply from AI model.",
+                    censorAWSAccountNumber(
+                        System.getenv("AWS_BEDROCK_MODEL_ID")
+                    ),
+                    prompt,
+                    imageContentType,
+                    keywords
+                ));
+
+        } catch (NoSuchKeyException ignored) {
+            throw new HttpPreconditionFailedException("No image uploaded for meme.");
+        } catch (IOException e) {
+            throw new ServerErrorException("Meme could not be retrieved.", e);
+        }
+    }
+
+    private static Set<String> getKeywordSuggestions(
+        byte[] bytes, @Nullable String contentType
+    ) {
+        var imageFormat = switch (contentType) {
             case MediaType.IMAGE_PNG_VALUE -> ImageFormat.PNG;
             case MediaType.IMAGE_JPEG_VALUE -> ImageFormat.JPEG;
-            case null, default -> throw new UnsupportedMediaTypeStatusException(
-                "Only JPEG and PNG files are allowed."
+            case null, default -> throw new ServerErrorException(
+                "Uploaded meme has illegal content type.",
+                new UnsupportedMediaTypeStatusException(
+                    "Only JPEG and PNG files are allowed."
+                )
             );
         };
 
-        var prompt = """
-         You are an expert at analyzing images.
-         Extract the most relevant keywords that best represent the image’s content.
-        
-         Respond with a comma-separated list of keywords, with no extra text.
-         Example: foo, bar, baz""";
-
         var systemMessage = SystemContentBlock.builder().text(prompt).build();
+        var imageSource = ImageSource.builder()
+                .bytes(SdkBytes.fromByteArray(bytes))
+                .build();
+
+        var imageBlock = ImageBlock.builder()
+                .source(imageSource)
+                .format(imageFormat)
+                .build();
+
+        var imageMessage = Message.builder()
+                .content(ContentBlock.fromImage(imageBlock))
+                .role(ConversationRole.USER)
+                .build();
 
         try (var client = BedrockRuntimeClient.builder().build()) {
             try {
-                var imageSource = ImageSource.builder()
-                    .bytes(SdkBytes.fromByteArray(file.getBytes()))
-                    .build();
-
-                var imageBlock = ImageBlock.builder()
-                    .source(imageSource)
-                    .format(imageFormat)
-                    .build();
-
-                var imageMessage = Message.builder()
-                    .content(ContentBlock.fromImage(imageBlock))
-                    .role(ConversationRole.USER)
-                    .build();
-
                 ConverseResponse response = client.converse(request -> request
                     .modelId(System.getenv("AWS_BEDROCK_MODEL_ID"))
                     .system(systemMessage)
@@ -71,37 +152,26 @@ public class KeywordController {
                         .temperature(0.5F)
                         .topP(0.9F)));
 
-                var responseText = response.output().message().content().getFirst().text();
-                var keywords = Arrays
-                    .stream(responseText.split(","))
-                    .map(s -> s.trim().toLowerCase())
-                    // Sometimes, the LLM will add a period at the end of the
-                    // keyword (possibly because it is the last in the list?).
-                    // So we remove it here if that is the case.
-                    .map(s -> s.endsWith(".") ? s.substring(0, s.length() - 1) : s)
-                    .collect(Collectors.toSet());
-
-                return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(new KeywordSuggestionResponse(
-                        "Received reply from AI model.",
-                        censorAWSAccountNumber(
-                            System.getenv("AWS_BEDROCK_MODEL_ID")
-                        ),
-                        prompt,
-                        file.getContentType(),
-                        file.getOriginalFilename(),
-                        keywords
-                    ));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                var content = response.output().message().content();
+                if (content.isEmpty()) {
+                    return Set.of();
+                } else {
+                    return Arrays
+                        .stream(content.getFirst().text().split(","))
+                        .map(s -> s.trim().toLowerCase())
+                        // Sometimes, the LLM will add a period at the end of the
+                        // keyword (possibly because it is the last in the list?).
+                        // So we remove it here if that is the case.
+                        .map(s -> s.endsWith(".") ? s.substring(0, s.length() - 1) : s)
+                        .collect(Collectors.toSet());
+                }
             } catch (ThrottlingException e) {
                 throw new HttpTooManyRequestsException();
             }
         }
     }
 
-    public static String censorAWSAccountNumber(String arn) {
+    private static String censorAWSAccountNumber(String arn) {
         // Split the ARN into segments by colons
         String[] segments = arn.split(":");
 
--- src/main/java/com/ageulin/mmm/dtos/responses/KeywordSuggestionResponse.java
+++ src/main/java/com/ageulin/mmm/dtos/responses/KeywordSuggestionResponse.java
@@ -2,13 +2,14 @@ package com.ageulin.mmm.dtos.responses;
 
 import lombok.Getter;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Getter
 public class KeywordSuggestionResponse extends BaseResponse {
     record Input(
         String type,
-        String filename
+        Optional<String> filename
     ) {}
     private final String modelId;
     private final String prompt;
@@ -26,8 +27,23 @@ public class KeywordSuggestionResponse extends BaseResponse {
         super(message);
         this.modelId = modelId;
         this.prompt = prompt;
-        this.input = new Input(inputType, inputFilename);
+        this.input = new Input(inputType, Optional.of(inputFilename));
         this.reply = reply;
     }
+
+    public KeywordSuggestionResponse(
+            String message,
+            String modelId,
+            String prompt,
+            String inputType,
+            Set<String> reply
+    ) {
+        super(message);
+        this.modelId = modelId;
+        this.prompt = prompt;
+        this.input = new Input(inputType, Optional.empty());
+        this.reply = reply;
+    }
+
 
 }
